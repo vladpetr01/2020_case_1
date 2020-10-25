bb0ab58c0e9b75b74bb4b40b04a8e061f83e5133
vladpetr01 <55372941+vladpetr01@users.noreply.github.com>
2020-10-25 17:38:57 +0700

    Update case.py

--- 2020_case_1-main/case.py
+++ 2020_case_1-main/case.py
@@ -1,1 +1,19 @@
 import turtle as tl
+import math
+
+
+def triangle(x, y, z, color):
+    tl.up()
+    tl.goto(x, y)
+    tl.down()
+    tl.color(color)
+    tl.begin_fill()
+    tl.left(45)
+    tl.fd(z * math.sqrt(2) / 2)
+    tl.left(135)
+    tl.fd(z)
+    tl.left(135)
+    tl.fd(z * math.sqrt(2) / 2)
+    tl.end_fill()
+    
+tl.done()
